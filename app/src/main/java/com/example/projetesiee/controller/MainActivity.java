c29c324583385ae6837950c6199b89cab789f9b5
300FR <60340994+300FR@users.noreply.github.com>
2021-11-17 11:11:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetesiee/controller/MainActivity.java
@@ -0,0 +1,187 @@
+package com.example.projetesiee.controller;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.projetesiee.R;
+import com.example.projetesiee.model.User;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_GAME_ACTIVITY = 11;
+
+    private static final String SHARED_PREF_USER_INFO = "SHARED_PREF_USER_INFO";
+    private static final String SHARED_PREF_USER_INFO_LANGUAGE = "SHARED_PREF_LANGUAGE";
+    private static final String SHARED_PREF_USER_INFO_NAME = "SHARED_PREF_USER_INFO_NAME";
+    private static final String SHARED_PREF_USER_INFO_SCORE = "SHARED_PREF_USER_INFO_SCORE";
+    private static final String SHARED_PREF_USER_INFO_BEST_SCORE = "SHARED_PREF_USER_INFO_BEST_SCORE";
+
+    private TextView mGreetingTextView;
+    private TextView mUserName;
+    private TextView mUserBestScore;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+    private Button mRestartButton;
+    private Spinner mSpinnerLangues;
+
+    private final String[] langues = {"en","fr","us"};
+
+    private User mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGreetingTextView = findViewById(R.id.main_textview_bienvenu);
+        mNameEditText = findViewById(R.id.main_edittext_nom);
+        mPlayButton = findViewById(R.id.main_button_play);
+        mUserName = findViewById(R.id.main_textView_name);
+        mUserBestScore = findViewById(R.id.main_textview_score);
+        mRestartButton = findViewById(R.id.main_button_restartProfile);
+        mSpinnerLangues = findViewById(R.id.spinner_langues);
+
+        mSpinnerLangues.setAdapter(new ArrayAdapter<>(this,R.layout.spinner_item,langues));
+        String langueSave=getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).getString(SHARED_PREF_USER_INFO_LANGUAGE, null);
+        if (langueSave!=null) mSpinnerLangues.setSelection(getIndexLangue(langueSave));
+
+        mSpinnerLangues.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Adapter adapter = parent.getAdapter();
+                String langue = (String) adapter.getItem(position);
+                String langueSave=getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).getString(SHARED_PREF_USER_INFO_LANGUAGE, null);
+                if (langueSave!=null && langue.equals(langueSave)) return;
+
+                Locale locale = new Locale(langue);
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config,
+                        getBaseContext().getResources().getDisplayMetrics());
+
+                getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).edit()
+                        .putString(SHARED_PREF_USER_INFO_LANGUAGE, langue).apply();
+
+                finish();
+                overridePendingTransition(0, 0);
+                startActivity(getIntent());
+                overridePendingTransition(0, 0);
+            }
+            @Override public void onNothingSelected(AdapterView<?> parent) { }
+        });
+
+
+        mUser = new User();
+        sameUser();
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = mNameEditText.getText().toString();
+                getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                        .edit()
+                        .putString(SHARED_PREF_USER_INFO_NAME, name)
+                        .apply();
+                mUser.setFirstName(name);
+
+                startActivityForResult(new Intent(MainActivity.this, GameActivity.class),REQUEST_CODE_GAME_ACTIVITY);
+            }
+        });
+
+        mRestartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                        .edit()
+                        .putString(SHARED_PREF_USER_INFO_NAME, "")
+                        .apply();
+                getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                        .edit()
+                        .putInt(SHARED_PREF_USER_INFO_BEST_SCORE, 0)
+                        .apply();
+                sameUser();
+            }
+        });
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {  }
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {  }
+
+            @Override public void afterTextChanged(Editable s) {
+                mPlayButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (REQUEST_CODE_GAME_ACTIVITY == requestCode && RESULT_OK == resultCode && data != null) {
+           int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
+
+            getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE)
+                    .edit()
+                    .putInt(SHARED_PREF_USER_INFO_SCORE, score)
+                    .apply();
+            sameUser();
+
+        }
+    }
+
+    private int getIndexLangue(String s){
+        int index=0;
+        for(String l : langues){
+            if (s.equals(l)) break;
+            index++;
+        }
+        if (index==langues.length) index=0;
+        return index;
+    }
+
+    private void sameUser() {
+        String firstName = getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).getString(SHARED_PREF_USER_INFO_NAME, null);
+        int score = getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).getInt(SHARED_PREF_USER_INFO_SCORE, -1);
+        int bestScore=getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).getInt(SHARED_PREF_USER_INFO_BEST_SCORE, 0);
+
+        if (firstName!=null && !firstName.isEmpty()) {
+            if (score != -1) {
+                mGreetingTextView.setText(getString(R.string.main_welcome_name) +" "+ firstName +" "+ getString(R.string.main_welcome_score) +" "+ score);
+                if (bestScore<score){
+                    bestScore=score;
+                    getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE).edit()
+                            .putInt(SHARED_PREF_USER_INFO_BEST_SCORE, score).apply();
+                }
+            } else {
+                mGreetingTextView.setText(getString(R.string.main_welcome_name) +" "+ firstName);
+            }
+            mNameEditText.setSelection(mNameEditText.getText().length());
+            mPlayButton.setEnabled(true);
+        }else{
+            firstName="";
+            mPlayButton.setEnabled(false);
+            mGreetingTextView.setText(getString(R.string.main_welcome));
+        }
+        mNameEditText.setText(firstName);
+        mUserBestScore.setText(""+bestScore);
+        mUserName.setText(firstName);
+    }
+}
---
+++ app/src/main/java/com/example/projetesiee/model/User.java
@@ -0,0 +1,14 @@
+package com.example.projetesiee.model;
+
+public class User {
+
+    private String mFirstName;
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String mFirstName) {
+        this.mFirstName = mFirstName;
+    }
+}
